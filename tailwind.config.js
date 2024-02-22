f8073380503b351493b746b24d8749c03682b461
Eliso Saladze <eliso.saladze5@gmail.com>
2024-02-22 21:40:46 +0400

    details

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        unauth: "url(assets/bg.png)",
+      },
+      boxShadow: {
+        lg: "-10px -10px 20px 0px rgba(0, 0, 0, 0.25)",
+      },
+      colors: {
+        custom: {
+          inputBorder: "#F4A448",
+          inputBackground: "#FFF2E2",
+          main: "#143C57",
+        },
+      },
+    },
   },
   plugins: [],
 };
